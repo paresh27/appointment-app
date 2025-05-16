bdc20cd6abe360ddae726054bfeb6ab3c75043c6
Paresh Maheshwari <kumarpareshmaheshwari@gmail.com>
2025-05-16 18:05:52 +0530

    feat: Adding the appointment api routes.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AppointmentController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\HealthcareProfessionalController;
 use Illuminate\Support\Facades\Route;
@@ -12,4 +13,11 @@ Route::controller(AuthController::class)->group(function () {
 
 Route::controller(HealthcareProfessionalController::class)->group(function () {
     Route::get('healthcare-professionals/', 'index');
+})->middleware('auth:sanctum');
+
+Route::controller(AppointmentController::class)->group(function () {
+    Route::post('appointments/', 'store');
+    Route::get('appointments/', 'index');
+    Route::patch('appointments/{appointment}/cancel', 'cancel');
+    Route::patch('appointments/{appointment}/complete', 'complete');
 })->middleware('auth:sanctum');
